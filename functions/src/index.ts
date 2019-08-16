507b56840e812294e7b0acbb05dd4bf76d94d0db
Thomas Maier <maier.thomas94@googlemail.com>
2019-08-16 13:03:45 +0200

    Fixing Time Utilities

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,6 +6,7 @@ import { TData, TMeasurement } from './types'
 
 // Utility
 import { isToday, getCurrentTime } from './utility'
+
 //----------------------------------------------------
 admin.initializeApp()
 const MAX_LOG_COUNT = 3000
@@ -17,20 +18,27 @@ export const onDataUpdate = functions.database
     const deviceID = context.params.deviceID
     const after: TData = change.after.val()
 
-    console.log('Change happened to Device with ID: ', deviceID)
-    console.log('After Object: ', after)
+    console.log('Durchlauf zum Zeitpunkt ' + getCurrentTime() + ': ', {
+      deviceID: deviceID,
+      afterObject: after,
+      isToday: isToday(after.lastUpdated)
+    })
 
-    if (!isToday(after.lastUpdated))
-      return change.after.ref.update({ lastUpdated: getCurrentTime() })
-
-    switch (after.type) {
-      case 'sensor':
-        return updateSensor(deviceID, after.lastUpdated, after.values)
-      default:
-        return
-    }
+    if (isToday(after.lastUpdated)) return updateHistory(deviceID, after)
+    else return change.after.ref.update({ lastUpdated: getCurrentTime() })
   })
 
+//--------------------------------------------------------
+const updateHistory = async (deviceID: string, after: TData) => {
+  switch (after.type) {
+    case 'sensor':
+      return updateSensor(deviceID, after.lastUpdated, after.values)
+    default:
+      return
+  }
+}
+
+//--------------------------------------------------------
 const updateSensor = async (deviceID: string, timestamp: string, values: any) => {
   return admin
     .database()
@@ -41,11 +49,8 @@ const updateSensor = async (deviceID: string, timestamp: string, values: any) =>
       if (snapshot.hasChildren()) {
         // History for the Device already exists
         const oldDeviceHistory = snapshot.val()
-        console.log('Device History: ', oldDeviceHistory)
-
         Object.keys(values).forEach((key: string) => {
           const historyForKey: Array<TMeasurement> = oldDeviceHistory[key]
-          console.log(`History for ${key}: `, historyForKey)
 
           if (historyForKey.length > MAX_LOG_COUNT) historyForKey.splice(0, historyForKey.length)
 
@@ -53,10 +58,8 @@ const updateSensor = async (deviceID: string, timestamp: string, values: any) =>
           newDeviceHistory = { ...newDeviceHistory, [key]: historyForKey }
         })
       } else {
-        // no History logged for the device
+        // No History logged for the device yet
         Object.keys(values).forEach((key: string) => {
-          console.log('Current Key: ', key)
-
           newDeviceHistory = {
             ...newDeviceHistory,
             [key]: [{ timestamp: timestamp, value: values[key] }]
--- functions/src/utility.ts
+++ functions/src/utility.ts
@@ -4,9 +4,19 @@ export const isToday = (timestamp: string) => {
   if (timestamp === '') return false
 
   const formattedTimestamp = moment.unix(parseInt(timestamp))
-  return formattedTimestamp.isSame(moment(), 'days') && formattedTimestamp.isSame(moment(), 'year')
+  const currentTime = moment(moment.now())
+
+  console.log('Time-Compare: ', {
+    time: getCurrentTime(),
+    differenceToNow: formattedTimestamp.diff(currentTime),
+    days: formattedTimestamp.isSame(currentTime, 'day'),
+    months: formattedTimestamp.isSame(currentTime, 'month'),
+    years: formattedTimestamp.isSame(currentTime, 'year')
+  })
+
+  return formattedTimestamp.isSame(currentTime, 'day')
 }
 
 export const getCurrentTime = (): string => {
-  return moment.now().toString()
+  return (moment.now() / 1000).toString()
 }
